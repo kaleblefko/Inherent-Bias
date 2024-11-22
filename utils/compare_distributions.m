9529f7e36816975b5a0fca48f29287b8d76150b4
Kaleb <104028332+kaleblefko@users.noreply.github.com>
2024-11-22 17:33:58 -0500

    Remove nans prior to ranksum to avoid errors

--- utils/compare_distributions.m
+++ utils/compare_distributions.m
@@ -1,6 +1,8 @@
 function [h_ttest, p_ttest, p_ranksum, h_ranksum]=compare_distributions(dataset, y)
     c1_pairwise_distances = pdist(dataset(:,(y==1))');
     c2_pairwise_distances = pdist(dataset(:,(y==-1))');
+    c1_clean = c1_pairwise_distances(~isnan(c1_pairwise_distances));
+    c2_clean = c2_pairwise_distances(~isnan(c2_pairwise_distances));
     [h_ttest,p_ttest] = ttest2(c1_pairwise_distances, c2_pairwise_distances);
-    [p_ranksum, h_ranksum] = ranksum(c1_pairwise_distances, c2_pairwise_distances);
+    [p_ranksum, h_ranksum] = ranksum(c1_clean, c2_clean);
 end
