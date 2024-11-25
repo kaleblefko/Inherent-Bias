018b8f5307b711e8124ebee072d378859353a380
Kaleb <104028332+kaleblefko@users.noreply.github.com>
2024-11-25 16:10:17 -0500

    Changed titles of latent space plots
    Input layer has it's own title, hidden layers have unique titles based off layer num.

--- utils/plot_latent_space.m
+++ utils/plot_latent_space.m
@@ -1,7 +1,11 @@
 function f=plot_latent_space(Y, y, plot_num)
     f = figure(plot_num);
     gscatter(Y(:,1), Y(:,2), y, 'br', '..', 10);
-    title('t-SNE Visualization of Latent Space');
+    if plot_num == 1
+        title('t-SNE Visualization of Input Space');
+    elseif plot_num ~= 1
+        title(sprintf('t-SNE Visualization of Latent Space of Hidden Layer %d', plot_num-1));
+    end
     xlabel('t-SNE Dimension 1');
     ylabel('t-SNE Dimension 2');
     legend('Location','southeastoutside');
