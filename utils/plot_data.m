2ec4e29cc2962c832e2cf9e2c7f7f76000bf4829
Kaleb <104028332+kaleblefko@users.noreply.github.com>
2024-11-26 11:55:29 -0500

    Updated graph formatting and added legends

--- utils/plot_data.m
+++ utils/plot_data.m
@@ -1,15 +1,25 @@
 function plot_data(C1, C2, dimensions)
-    figure(1);
     if dimensions==2
-        scatter(C1(1,:), C1(2,:), MarkerEdgeColor="red")
+        figure(1);
+        scatter(C1(1,:), C1(2,:), MarkerEdgeColor="red", DisplayName='1')
         hold on
-        scatter(C2(1,:), C2(2,:), MarkerEdgeColor="blue")
+        scatter(C2(1,:), C2(2,:), MarkerEdgeColor="blue", DisplayName='-1')
         xlabel("x1")
         ylabel("x2")
+        legend('Location','southeastoutside');
+        grid on
+        title('2 Dimensional Input Data')
     elseif dimensions==3
+        figure(1);
         hold on
-        scatter3(C1(1,:),C1(2,:),C1(3,:), 'r')
-        scatter3(C2(1,:),C2(2,:),C2(3,:), 'b')
+        scatter3(C1(1,:),C1(2,:),C1(3,:), 'r', DisplayName='1')
+        scatter3(C2(1,:),C2(2,:),C2(3,:), 'b', DisplayName='-1')
+        xlabel("x1")
+        ylabel("x2")
+        zlabel("x3")
+        legend('Location','southeastoutside');
+        grid on
+        title('3 Dimensional Input Data')
     else
         X = [C1 C2];
         y = [ones(1,size(C1,2))*1 ones(1,size(C2,2))*-1];
