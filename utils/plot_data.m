6e0f1af6ff4c58600818472fb0745264e36a6be1
Kaleb <104028332+kaleblefko@users.noreply.github.com>
2024-11-25 21:57:25 -0500

    Added 3 dimensional plotting

--- utils/plot_data.m
+++ utils/plot_data.m
@@ -6,6 +6,10 @@ function plot_data(C1, C2, dimensions)
         scatter(C2(1,:), C2(2,:), MarkerEdgeColor="blue")
         xlabel("x1")
         ylabel("x2")
+    elseif dimensions==3
+        hold on
+        scatter3(C1(1,:),C1(2,:),C1(3,:), 'r')
+        scatter3(C2(1,:),C2(2,:),C2(3,:), 'b')
     else
         X = [C1 C2];
         y = [ones(1,size(C1,2))*1 ones(1,size(C2,2))*-1];
