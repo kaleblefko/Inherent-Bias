d76332b29ab8c11c9363e9ff6b37dc43227ef8dd
Kaleb <104028332+kaleblefko@users.noreply.github.com>
2024-11-26 19:16:49 -0500

    Now the covariance of the classes are the same

--- utils/produce_single_mode_set.m
+++ utils/produce_single_mode_set.m
@@ -1,11 +1,11 @@
-function [C1, C2]=produce_single_mode_set(l, m_minmax_c1, m_minmax_c2, cov_minmax_c1, cov_minmax_c2, diag_cov, N1_minmax, N2_minmax)
-
+function [C1, C2]=produce_single_mode_set(l, m_minmax_c1, m_minmax_c2, cov_minmax, diag_cov, N1_minmax, N2_minmax)
     N1 = randi([N1_minmax(1) N1_minmax(2)],1);
     N2 = randi([N2_minmax(1) N2_minmax(2)],1);
 
-    [m1, cov1] = produce_m_and_cov(l, m_minmax_c1, cov_minmax_c1, true, 1);
-    [m2, cov2] = produce_m_and_cov(l, m_minmax_c2, cov_minmax_c2, true, 1);
+    m1 = produce_random_mean(l, m_minmax_c1(1), m_minmax_c1(2));
+    m2 = produce_random_mean(l, m_minmax_c2(1), m_minmax_c2(2));
+    cov = produce_random_cov(l, cov_minmax(1), cov_minmax(2), diag_cov);
 
-    C1 = mvnrnd(m1, cov1, N1)';
-    C2 = mvnrnd(m2, cov2, N2)';
+    C1 = mvnrnd(m1, cov, N1)';
+    C2 = mvnrnd(m2, cov, N2)';
 end
