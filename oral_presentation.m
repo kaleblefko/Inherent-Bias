f4ac4735f16c5ee1bb2147a2d18d86b124ee6b5c
Kaleb <104028332+kaleblefko@users.noreply.github.com>
2024-11-27 14:21:07 -0500

    Only analyze hidden layers, not the output layer

--- oral_presentation.m
+++ oral_presentation.m
@@ -2,8 +2,8 @@ addpath("./utils");
 
 close all;clear all;clc
 
-load("Examples/example_1/C1.mat", "C1");
-load("Examples/example_1/C2.mat", "C2");
-load("Examples/example_1/net.mat", "net");
+load("Good/perfect/C1.mat", "C1");
+load("Good/perfect/C2.mat", "C2");
+load("Good/perfect/net.mat", "net");
 
-generate_visualization(C1, C2, net, net.numLayers, 3);gith
+generate_visualization(C1, C2, net, net.numLayers-1, 3);
