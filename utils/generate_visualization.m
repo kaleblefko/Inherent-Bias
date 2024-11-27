faedd85a35342856daa0397dd6810fc3a05f7fc3
Kaleb <104028332+kaleblefko@users.noreply.github.com>
2024-11-27 14:21:36 -0500

    Analyze input layer and add textbox for distance ratio

--- utils/generate_visualization.m
+++ utils/generate_visualization.m
@@ -2,7 +2,8 @@ function [] = generate_visualization(C1, C2, net, num_hidden_layers, input_data_
     [idx1C1, idx2C1] = get_random_pair_indices(C1);
     [idx1C2, idx2C2] = get_random_pair_indices(C2);
     idxs = [idx1C1, idx2C1, idx1C2, idx2C2];
-
+    
+    figure(1);
     hold on
     Y = [C1 C2];
     X = Y;
@@ -12,8 +13,16 @@ function [] = generate_visualization(C1, C2, net, num_hidden_layers, input_data_
     end
     plot_data(Y(:, y==1), Y(:, y==-1), 2);
     equalize_axes();
-    plot_pairs(Y(:, y==1), Y(:, y==-1), idxs);
+    ratio = plot_pairs(Y(:, y==1), Y(:, y==-1), C1, C2, idxs);
+    annotation('textbox',...
+    [0.700999999999999 0.309523809523813 0.240071428571429 0.06666666666667],...
+    'String',sprintf('Pair Distance Ratio = %.2f', ratio),...
+    'FitBoxToText','off',...
+    'BackgroundColor',[1 1 1]);
     hold off
+
+    disp('Analyzing input data...');
+    [h_ttest, p_ttest, p_ranksum, h_ranksum] = compare_distributions(X,y)
         
     analyze_network(net, X, y, num_hidden_layers, true, true, idxs);
 end
