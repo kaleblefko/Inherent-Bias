8fea0a10d40373217c4c4d5791b4ae5f1905d41f
Kaleb <104028332+kaleblefko@users.noreply.github.com>
2024-12-03 13:17:11 -0500

    Set default background of figs to 1

--- oral_presentation.m
+++ oral_presentation.m
@@ -2,9 +2,11 @@ addpath("./utils");
 
 close all;clear all;clc
 
-load("saved_data/perfect/C1.mat", "C1");
-load("saved_data/perfect/C2.mat", "C2");
-load("saved_data/perfect/net.mat", "net");
+set(0,'defaultfigurecolor',[1 1 1])
+
+load("Good/perfect/C1.mat", "C1");
+load("Good/perfect/C2.mat", "C2");
+load("Good/perfect/net.mat", "net");
 
 [idxs, ratios] = generate_visualization(C1, C2, net, net.numLayers-1, 3, true);
-disp(ratios)
+disp(ratios);
